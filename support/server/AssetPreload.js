03b1fabdb6f49f11dd2f77e034dd71abdd8d804c
Nikolay Suslov <nsuslovi@gmail.com>
2015-12-09 01:09:59 +0300

    fix assetpreload

--- support/server/AssetPreload.js
+++ support/server/AssetPreload.js
@@ -109,7 +109,7 @@ function parseStateForAssets(state,cb)
 	//
 	// 3) The User selects one or more assets from the list of assets in the properties section of the scene editor
 	//
-	if(state !== null) {
+	if(state !== null || state !== undefined) {
 		// Load assets added manually to the state file
 		var additionalAssets = state[state.length - 1]['___additionalAssets'];
 		if (additionalAssets !== undefined) {
